10eb89b9aa2359372f3b751e6bfb472e5406d55e
2215000306Anujkumar <142488290+2215000306Anujkumar@users.noreply.github.com>
2025-01-27 23:44:59 +0530

    Time: 28 ms (6.46%) | Memory: 55.7 MB (97.13%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,15 +1,18 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        HashMap<Integer , Integer> map = new HashMap<>();
         List<Integer> result = new ArrayList<>();
-        for(int i=0 ; i<nums.length ; i++){
-            int idx = Math.abs(nums[i])-1;
-            if(nums[idx] < 0)
-                continue;
-            nums[idx] *=-1; 
+
+        for(int num : nums){
+            if(map.containsKey(num)){
+                map.put(num , map.get(num) + 1);
+            }else{
+                map.put(num,1);
+            }
         }
-        for(int i=0 ; i<nums.length ; i++){
-            if(nums[i] > 0){
-                result.add(i+1);
+        for(int i=1 ; i<=nums.length ;i++){
+            if(!map.containsKey(i)){
+                result.add(i);
             }
         }
         return result;
