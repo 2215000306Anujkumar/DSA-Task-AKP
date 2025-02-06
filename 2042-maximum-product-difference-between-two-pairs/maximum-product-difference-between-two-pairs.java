76d1a8c146dd859136a8471935d86f05e6a51e44
2215000306Anujkumar <142488290+2215000306Anujkumar@users.noreply.github.com>
2025-02-06 11:36:02 +0530

    Time: 2 ms (98.99%) | Memory: 45.1 MB (54.92%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -3,13 +3,13 @@ class Solution {
         int l1 = 0 , l2 = 0;
         int s1 = Integer.MAX_VALUE , s2 = Integer.MAX_VALUE;
         for(int num : nums){
-            if(num > l1){
+            if(num >= l1){
                 l2 = l1;
                 l1 = num;
             }else if(num > l2){
                 l2 = num;
             }
-            if(num < s1){
+            if(num <= s1){
                 s2 = s1;
                 s1 = num;
             }else if(num < s2){
