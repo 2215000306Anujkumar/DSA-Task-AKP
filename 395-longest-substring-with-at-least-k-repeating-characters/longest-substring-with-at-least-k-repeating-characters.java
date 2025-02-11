9f8f1b08ab5269bed1e56967f4b7f0115bd8c9c5
2215000306Anujkumar <142488290+2215000306Anujkumar@users.noreply.github.com>
2025-02-11 22:00:26 +0530

    Time: 64 ms (25.13%) | Memory: 43.6 MB (26.45%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int call(String s , int start, int end , int k){
+        if(end - start < k) return 0;
+
+        int freq[]  = new int[26];
+        for( int i=start ; i<end ; i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        for(int i=start ; i<end ; i++){
+            if(freq[s.charAt(i)-'a'] <k){
+                int left = call(s,start,i,k);
+                int right = call(s,i+1,end,k);
+                return Math.max(left,right);
+            }
+        }
+        return end - start;
+    }
+    public int longestSubstring(String s, int k) {
+        return call (s,0,s.length(),k);
+    }
+    
+}
