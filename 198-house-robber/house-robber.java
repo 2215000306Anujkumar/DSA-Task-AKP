98404f9307016bd46a3e7704d63060d3fb7639eb
2215000306Anujkumar <142488290+2215000306Anujkumar@users.noreply.github.com>
2025-06-14 15:20:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (47.02%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,34 +1,52 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length ;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
+        // int n = nums.length;
+        // int[] dp = new int[n];
+        // Arrays.fill(dp, -1);
 
-       return find(nums ,n-1 ,dp);
+        // return find(nums, n - 1, dp);
+        return find2(nums);
 
-
-        
     }
-    int find(int[] nums , int i , int[] dp ){
-        if(i<0){
+
+    int find(int[] nums, int i, int[] dp) {
+        if (i < 0) {
             return 0;
         }
 
-        if(dp[i] != -1){
+        if (dp[i] != -1) {
             return dp[i];
         }
 
+        int taken = nums[i] + find(nums, i - 2, dp);
+        int nottaken = find(nums, i - 1, dp);
 
+        return dp[i] = Math.max(taken, nottaken);
 
-        int taken = nums[i] + find(nums , i -2 , dp);
-        int nottaken = find(nums , i-1 ,dp);
+    }
 
-        return dp[i] =  Math.max(taken , nottaken);
+  int  find2(int[] nums){
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        if(n == 1) return dp[0];
+        dp[1] = Math.max(nums[0] , nums[1]);
+
+        for(int i = 2 ; i < n ; i++){
+            int taken = nums[i] +  dp[i-2];
+            int nottaken = dp[i-1];
+
+            dp[i] =  Math.max(taken , nottaken);
+                            
+
+        }
+
+        return dp[n-1];
+
 
 
 
 
     }
-
 
 }
