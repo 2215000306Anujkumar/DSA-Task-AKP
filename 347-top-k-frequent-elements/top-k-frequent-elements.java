b5186974b109ef70bbccd6494adcd42885f28659
2215000306Anujkumar <142488290+2215000306Anujkumar@users.noreply.github.com>
2025-07-01 09:04:58 +0530

    Time: 17 ms (13.41%) | Memory: 48.3 MB (76.62%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -3,16 +3,15 @@ class Solution {
         HashMap<Integer , Integer> map = new HashMap<>();
         for(int num : nums){
             if(map.containsKey(num)){
-                map.put(num , map.get(num) + 1);
+                map.put(num , map.get(num)+1);
             }else{
                 map.put(num , 1);
             }
         }
-
         List<Integer> list = new ArrayList<>(map.keySet());
-        list.sort((a, b) -> map.get(b) - map.get(a));
+        list.sort((a,b) -> map.get(b) - map.get(a));
         int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
+        for(int i=0 ; i<k ; i++){
             ans[i] = list.get(i);
         }
         return ans;
